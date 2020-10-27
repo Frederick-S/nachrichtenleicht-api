1cc3f532f8f0013190841663a32705a8d816c2ba
Frederick-S <mao_xiaodan@hotmail.com>
2020-10-27 20:36:31 +0800

    init

---
+++ src/main/kotlin/nachrichtenleicht/NachrichtenleichtApplication.kt
@@ -0,0 +1,11 @@
+package nachrichtenleicht
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NachrichtenleichtApplication
+
+fun main(args: Array<String>) {
+	runApplication<NachrichtenleichtApplication>(*args)
+}
---
+++ src/test/kotlin/nachrichtenleicht/NachrichtenleichtApplicationTests.kt
@@ -0,0 +1,11 @@
+package nachrichtenleicht
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NachrichtenleichtApplicationTests {
+	@Test
+	fun contextLoads() {
+	}
+}
